b84eb9123fc1209c6f76521b07c5ef273019c2e8
reudej <reuel@email.cz>
2025-12-10 20:48:46 +0100

    Dovytvoření základní struktury projektu

--- pyvafgen.sh
+++ pyvafgen.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 echo "VNVDIR=\"$(pwd)/$1\"" > rc
+echo "PRJDIR=\"$(pwd)/$2\"" >> rc
 cat >> rc << EOF
 a() {
     source \$VNVDIR/bin/activate
+}
+mng() {
+    \$PRJDIR/manage.py "\$@"
 }
 EOF
 
