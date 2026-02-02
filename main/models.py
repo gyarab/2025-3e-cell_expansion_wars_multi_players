4742c01a8ca63a9af936de92cf754fcfa4e32567
reudej <reuel@email.cz>
2026-02-02 22:16:06 +0100

    Vytvoření djnago modelů

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Level(models.Model):
+    data = models.JSONField(max_length=5000)
+
+class Playthrough(models.Model):
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    level = models.OneToOneField(
+        Level,
+        on_delete=models.SET_NULL,
+        related_name="playthrough",
+        null=True
+    )
+    players = models.ManyToManyField(
+        "Player",
+        through="Player_Playthrough",
+        related_name="playthroughs"
+    )
+
+class Player(models.Model):
+    email = models.CharField(max_length=35)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    password_hash = models.BinaryField(max_length=32)
+
+class Player_Playthrough(models.Model):
+    time_in_game = models.PositiveIntegerField()
+    real_time = models.DurationField()
+    playthrough = models.ForeignKey(Playthrough, on_delete=models.SET_NULL, null=True)
+    player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True)
