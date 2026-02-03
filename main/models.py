f17a72967c458f65637cf41869596359217ca316
reudej <reuel@email.cz>
2026-02-03 18:06:56 +0100

    Přidání dalších modelů do models.py + přjmenování 2 fieldů

--- main/models.py
+++ main/models.py
@@ -17,6 +17,7 @@ class Playthrough(models.Model):
         through="Player_Playthrough",
         related_name="playthroughs"
     )
+    game_state = models.JSONField(max_length=2500)
 
 class Player(models.Model):
     email = models.CharField(max_length=35)
@@ -25,7 +26,25 @@ class Player(models.Model):
     password_hash = models.BinaryField(max_length=32)
 
 class Player_Playthrough(models.Model):
-    time_in_game = models.PositiveIntegerField()
-    real_time = models.DurationField()
-    playthrough = models.ForeignKey(Playthrough, on_delete=models.SET_NULL, null=True)
-    player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True)
+    end_of_player_datetime = models.DateTimeField(null=True)
+    virt_time_in_game = models.PositiveIntegerField()
+    real_time_in_game = models.DurationField()
+    playthrough = models.ForeignKey(Playthrough, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+class GameAction(models.Model):
+    virt_time_in_game = models.PositiveIntegerField()
+    player = models.OneToOneField(
+        Player,
+        on_delete=models.SET_NULL,
+        related_name="game_action",
+        null=True
+    )
+    playthrough = models.ForeignKey(
+        Playthrough,
+        on_delete=models.CASCADE,
+        related_name="game_actions"
+    )
+    action_type = models.PositiveSmallIntegerField()
+    par1 = models.PositiveSmallIntegerField()
+    par2 = models.PositiveSmallIntegerField()
