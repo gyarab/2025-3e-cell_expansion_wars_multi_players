ed13db3bc312a649b3264cb04ba96cb98acf05b1
reudej <reuel@email.cz>
2026-02-23 13:51:07 +0100

    Začátek psaní views

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def get_whole_name(user):
+    return " ".join((user.first_name, user.last_name))
+
 # Create your views here.
+async def homepage(request):
+    name = get_whole_name(await request.auser())
+    
