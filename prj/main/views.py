894d8c7047e2e86aac5f3d26a4669ec2fa71fa84
reudej <reuel@email.cz>
2026-02-26 21:23:27 +0100

    Napsání view "change_user_info"

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import BadRequest
+from django.db import IntegrityError
 from django.contrib.auth import login, logout
 from django.shortcuts import render as render_
 from django.http import HttpResponse
@@ -13,10 +14,17 @@ def get_whole_name(user):
     names = [name for name in (user.first_name, user.last_name) if name != ""]
     return " ".join(names) if user.is_authenticated else None
 
+def check_method(request, m):
+    if request.method != m:
+        raise BadRequest()
+    
+
+success = HttpResponse("success", content_type="text/plain")
+fail = HttpResponse("fail", content_type="text/plain")
+
 # Create your views here.
 async def homepage(request):
-    if request.method != "GET":
-        raise BadRequest()
+    check_method(request, "GET")
 
     name = get_whole_name(await request.auser())
     
@@ -35,13 +43,12 @@ async def login_view(request):
         return render("login.html", request, data)
 
     if request.method == "POST":
-        data = request.read(84)    # Max: 40 chars email (max length of username is 30), 40 chars password, 4 chars 2x \r\n
+        data = request.read(164)    # Max: 40 chars email (max length of username is 30), 40 chars password, 4 chars 2x \r\n
         try:
-            login_str, passwd = data
+            login_str, passwd = data.strip().splitlines()
         except Exception:
             raise BadRequest()
 
-        fail = HttpResponse("fail", content_type="text/plain")
 
         try:
             if "@" in login_str:
@@ -53,17 +60,43 @@ async def login_view(request):
 
         if user.check_password(passwd):
             login(request, user)
-            return HttpResponse("success", content_type="text/plain")
+            return success
 
-        else: return fail
+        return fail
     
     raise BadRequest()
     
 async def logout_view(request, uid):
+    check_method(request, "POST")
     logout(request)
     return HttpResponse("success", content_type="text/plain")
 
 async def user_profile(request, uid):
+    check_method(request, "GET")
+
     player = await request.auser()
     data = {field: getattr(player, field) for field in ("email", "username", "first_name", "last_name")}
     return render("user_profile.html", request, data)
+
+async def change_user_info(request, uid):
+    check_method(request, "POST")
+
+    changable_fields = ("username", "first_name", "last_name")
+    changes = [line.split(":") for line in request.read(184).strip().splitlines()]
+    
+    for change in changes:
+        if len(change) != 2 or not change[0] in changable_fields:
+            raise BadRequest()
+
+    player = await request.auser()
+    
+    for change in changes:
+        setattr(player, *change)
+
+    try:
+        player.save()
+    except IntegrityError:
+        return fail
+    
+    return success
+
